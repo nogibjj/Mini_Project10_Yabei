160de548591f453c651ba13ec991122fc05e2c21
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-11-02 15:38:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 """
 Main application entry point for PySpark data processing
 """
+from mylib.lib import (
+    initiate_spark_session,
+    read_dataset,
+    categorize_origin,
+    append_to_report
+)
 
-from mylib.lib import create_spark_session, load_data, transform_data, run_sql_query
+def run_data_analysis():
+    spark = initiate_spark_session("Car Data Analysis")
+
+    data_file_path = "data/cars.csv"
+    car_data = read_dataset(spark, data_file_path)
+
+    categorized_data = categorize_origin(car_data)
+
+    categorized_data.createOrReplaceTempView("car_data_view")
+    query_result = spark.sql("""
+        SELECT RegionCategory, COUNT(*) AS TotalCars
+        FROM car_data_view
+        GROUP BY RegionCategory
+        ORDER BY TotalCars DESC
+    """)
+
+    query_result.show()
+    append_to_report("Spark SQL Query Result", query_result.limit(10).toPandas().to_markdown())
+
+    # query_result.write.format("csv").save("output/query_result.csv")
+
+    spark.stop()
 
 if __name__ == "__main__":
-    spark = create_spark_session("Car Data Analysis")
-
-    data_file_path = "data/cars.csv" 
-    df = load_data(spark, data_file_path)
-
-    transformed_df = transform_data(df)
-
-    result_df = run_sql_query(spark, transformed_df)
-
-    result_df.show()
-
-    # result_df.write.format("csv").save("output/average_weight_by_origin.csv")
+    run_data_analysis()
