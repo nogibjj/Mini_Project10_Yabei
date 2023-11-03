7f09806ed7f91686106de714c334b50195079c61
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-11-02 22:45:15 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,12 @@
 """
 PySpark Application Test Suite for Car Data
 """
+from pyspark.sql.session import SparkSession
 import pytest
 from mylib.lib import (
     initiate_spark_session,
     read_dataset,
+    describe,
     transform_origin,
 )
 
@@ -17,13 +19,17 @@ def spark_session():
     session.stop()
 
 
-def test_data_loading(spark_session):
+def test_data_loading(spark_session: SparkSession):
     data_path = "data/cars.csv"
     car_df = read_dataset(spark_session, data_path)
     assert car_df is not None and car_df.count() > 0
 
+def test_data_describe(spark_session: SparkSession):
+    car_df = read_dataset(spark_session, "data/cars.csv")
+    description_data = describe(car_df)
+    assert description_data is not None
 
-def test_data_transform(spark_session):
+def test_data_transform(spark_session: SparkSession):
     car_df = read_dataset(spark_session, "data/cars.csv")
     transformed_car_df = transform_origin(car_df)
     assert transformed_car_df is not None
@@ -32,6 +38,7 @@ def test_data_transform(spark_session):
 
 def run_tests():
     session = spark_session()
+    test_data_loading(session)
     test_data_loading(session)
     test_data_transform(session)
 
